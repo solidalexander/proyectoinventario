95f26c6a457c6ee2807fe9db47b201991013d7d0
solidalexander <solidalexander@gmail.com>
2022-09-09 18:56:45 -0500

    nuevos cambios en la sesion

--- router/inventario.js
+++ router/inventario.js
@@ -1,21 +1,24 @@
 const{ Router } = require('express');
-const router = Router ();
+
 const Inventario = require ('../modelo/Inventario');
+const { validarInventario } = require ('../Helpers/validarinventario')
+
+const router = Router ();
 
 router.get('/', async  function(req, res){
     try {
           const inventarios = await Inventario.find().populate([
              {
-                 path: 'usuario'
+                 path: 'usuario', select: 'nombre email estado'
              },
              {
-                 path: 'marca'
+                 path: 'marca',select: 'nombre  estado'
              },
              {
-                 path: 'tipoequipo'
+                 path: 'tipoequipo' ,select: 'nombre  estado'
              },
              {
-                 path: 'estadoequipo'
+                 path: 'estadoequipo',select: 'nombre  estado'
              }
  
           ]);
@@ -23,16 +26,21 @@ router.get('/', async  function(req, res){
  
     }catch(error) {
      console.log(error);
-     res.send('Ocurrio un error al consultar inventarios')
+     res.status(500).send('Ocurrio un error al consultar inventarios');
     }
  });
  
  router.post('/', async function(req, res){
      try {
+
+        const validaciones = validarInventario (req);
+        if(validaciones.length>0){
+            return res.status(400).send(validaciones)
+        }
  
          const existeInventarioporserial = await Inventario.findOne({serial: req.body.serial });
          if(existeInventarioporserial) {
-             return res.send('Ya existe el serial para otro Dispositivo');
+             return res.status (400).send('Ya existe el serial para otro Dispositivo') ;
  
          }
  
@@ -57,14 +65,49 @@ router.get('/', async  function(req, res){
  
    }catch(error) {
     console.log(error);
-    res.send('Ocurrio un error al consultar los inventarios')
+    res.send.status(500)('Ocurrio un error al consultar los inventarios');
    }
  
  });
  
- router.put('/', function(req, res){
-     res.send('estoy inventario put');
+ router.put('/InventarioId', async function(req, res){
+    try {
  
+        let inventario = await inventario.findById (req.params.inventarioId)
+        if (!inventario) { return res.send ('inventario no existe'). status (400);
+    }
+
+
+        const existeInventarioPorSerial = await Inventario
+                          .findOne({serial: req.body.serial,_id: {$ne: inventario._id }});
+        if(existeInventarioPorSerial) {
+            return res.send('Ya existe el serial para otro Dispositivo'). status (400);
+
+        }
+
+        
+        inventario.serial = req.body.serial;
+        inventario.modelo = req.body.modelo;
+        inventario.descripcion = req.body.descripcion;
+        inventario.foto = req.body.foto;
+        inventario.color = req.body.color;
+        inventario.fehaCompra = req.body.fechaCompra;
+        inventario.precio = req.body.preecio;
+        inventario.usuario = req.body.usuario._id;
+        inventario.marca = req.body.marca._id;
+        inventario.tipoEquipo = req.body.tipoEquipo._id;
+        inventario.estadoEquipo = req.body.estadoEquipo._id;
+        inventario.fechaActualizacion = new Date();
+
+        inventario = await inventario.save();
+        
+        res.send(inventario);
+    }
+    catch(error) {
+        console.log(error);
+        res.send('Ocurrio un error al consultar los inventarios'). status (500);
+       }
+
  });
 
 module.exports = router;
